8a51128cc8a8704fdbf8a553c3d60660d08d9e67
wangjun <444055828@qq.com>
2022-06-09 21:58:18 +0800

    20220609

--- components/upload.tsx
+++ components/upload.tsx
@@ -28,21 +28,59 @@ export const Upload = () => {
             setUris(old => [...old, result.uri]);
         }
     }
+    const del = (uri: string) => {
+        setUris(old => old.filter(u => u !== uri));
+    }
     return (
         <View style={styles.button_container}>
-            <TouchableOpacity style={styles.button} onPress={pick}><Text>图片库</Text></TouchableOpacity>
-            <TouchableOpacity style={styles.button} onPress={camera}><Text>拍摄</Text></TouchableOpacity>
+            <View style={styles.image_container}>
+                {
+                    uris.map(u => <View style={styles.preview} key={u}>
+                        <TouchableOpacity style={styles.delete_button} onPress={() => { del(u)}}><Text>X</Text></TouchableOpacity>
+                        <Image style={styles.image} source={{uri: u}}/>
+                        </View>)
+                }
+            </View>
+            <View style={styles.button_container}>
+                <TouchableOpacity style={styles.button} onPress={pick}><Text>图片库</Text></TouchableOpacity>
+                <TouchableOpacity style={styles.button} onPress={camera}><Text>拍摄</Text></TouchableOpacity>
+            </View>
         </View>
     )
 }
 
 const styles = StyleSheet.create({
     button_container: {
-        flexWrap: "wrap"
+        flexWrap: "wrap",
+        flexDirection: "row",
+        width: "100%",
+        height: "100%",
     },
     button: {
         width: "20%",
         height: "20%",
+    },
+    image_container: {
+        flexWrap: "wrap",
+        flexDirection: "row",
+        justifyContent: "space-around",
+        width: "100%",
+    },
+    preview: {
+        width: 150,
+        height: 150,
+    },
+    image: {
+       width: "100%",
+       height: "100%", 
+        borderRadius: 10,
+    },
+    delete_button: {
+        position: "absolute",
+        top: 10,
+        right: 10,
+        zIndex: 1,
     }
+
 })
 
