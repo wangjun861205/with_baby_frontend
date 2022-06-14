188adb2c6d3e1f38740a847752dcf08583e1df7b
wangjun <444055828@qq.com>
2022-06-14 22:00:32 +0800

    modify upload component

--- apis/upload.tsx
+++ apis/upload.tsx
@@ -1,12 +1,12 @@
 import { BASE_URL } from "@env";
 import { getJWTToken } from "../utils/jwt";
 
-export const upload = async (content: string | Blob): Promise<number> => {
+export const upload = async (content: string | Blob): Promise<number[]> => {
     const data = new FormData();
     data.append("file", content);
     const token = await getJWTToken();
-    const res = await fetch(BASE_URL + "/upload", {
-        method: "POST",
+    const res = await fetch(BASE_URL + "/api/upload", {
+        method: "post",
         headers: {
             JWT_TOKEN: token,
         },
--- screens/eating.tsx
+++ screens/eating.tsx
@@ -1,16 +1,19 @@
 import useAsync from "../hooks/async";
 import { createEating } from "../apis/eating";
 import { View, TextInput, Button } from "react-native";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { NativeStackNavigationProp } from "@react-navigation/native-stack";
 import { Upload } from "../components/upload";
 
 
 export const EatingCreate = ({ navigation, route }: { navigation: any, route: any }) => {
 	const [name, setName] = useState("");
-	const [images, setImages] = useState<string[]>([]);
+	const [images, setImages] = useState<number[]>([]);
+	useEffect(() => {
+		console.log(images);
+	}, [images])
 	return <View>
 		<TextInput placeholder="名称..." onChangeText={(t) => setName(t)}></TextInput>
-		<Upload />
+		<Upload setIDs={setImages}/>
 	</View>
 }
