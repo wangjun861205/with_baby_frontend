97126cc5699a6f379d934d6af38f6c43a327d5ac
wangjun <444055828@qq.com>
2022-06-20 21:19:09 +0800

    resolve upload blob problem

--- apis/upload.tsx
+++ apis/upload.tsx
@@ -3,6 +3,7 @@ import { getJWTToken } from "../utils/jwt";
 import * as FS from "expo-file-system";
 import { Buffer } from "buffer";
 
+
 interface File {
     name: string,
     size: number,
@@ -11,21 +12,14 @@ interface File {
 }
 
 export const upload = async (uri: string, token: string): Promise<number> => {
-    const content = await FS.readAsStringAsync(uri, { encoding: FS.EncodingType.Base64 });
-    const buffer = Buffer.from(content, 'base64');
-    const blob = new Blob([buffer]);
     const data = new FormData();
-    data.append("file", content);
+    data.append("file", {uri, name: "temp", type: "image/png"} as any);
     const res = await fetch(BASE_URL + "/api/upload", {
-        method: "post",
+        method: "POST",
         headers: {
             JWT_TOKEN: token,
         },
         body: data,
     });
-    if (res.status !== 200) {
-        console.error(res);
-        return Promise.reject(res.status);
-    }
     return Promise.resolve((await res.json())[0]);
 }
